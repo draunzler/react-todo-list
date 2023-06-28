5578d5bd5afac317ed7e1ecb77148b421a3dbfcd
Draunzler <shadymeister2017@gmail.com>
2023-06-29 01:55:29 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react"
 import "./style.css"
 
 export default function App(){
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e){
+    e.preventDefault()
+  }
+
   return (
     <>
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
+        <input 
+          value={newItem} 
+          onChange={e => setNewItem(e.target.value)} 
+          type="text" 
+          id="item" 
+        />
       </div>
       <button className="btn">Add</button>
     </form>
